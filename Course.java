a14f389c37ac3bd376faf8f650441132f2fe9c17
jcastillo2583 <bluejccastle@gmail.com>
2019-05-05 20:21:10 -0700

    Update Course.java

--- Course.java
+++ Course.java
@@ -3,36 +3,63 @@ import java.util.*;
 public class Course {
 	String name;
 	ArrayList<Book> availBooks = new ArrayList<Book>();
+	ArrayList<Book> loanedBooks = new ArrayList<Book>();
+	ArrayList<Book> allBooks = new ArrayList<Book>();
 	Boolean active;
 		
-	public Course(String name, ArrayList<Book> availBooks) {
+	public Course(String name, ArrayList<Book> availBooks, boolean active) {
 		this.name = name;
 		this.availBooks = availBooks;
+		this.allBooks = availBooks;
+		this.active = active;
 	}
-	
-	public Course(String name) {
+	public Course(String name, boolean active) {
 		this.name = name;	
+		this.active = active;
 	}
 	
 	
 	
 	public String getName() {
 		return name;	}
-	
-	public ArrayList<Book> getBooks() {
+	public ArrayList<Book> getAvailBooksList() {
 		return availBooks;	}
+	public ArrayList<Book> getLoanedBooksList() {
+		return loanedBooks;	}
+	public ArrayList<Book> getAllBooksList() {
+		return allBooks;	}
+	public boolean isActive() {
+		return active;	}
+	
 	
 	public void addBook(Book book) {
-		availBooks.add(book);	}
+		availBooks.add(book);	
+		allBooks.add(book);
+	}
+	public void removeBook(Book book) {
+		if(loanedBooks.contains(book))
+			throw new IllegalArgumentException("Book is currently loaned, it must be returned first.");
+		availBooks.remove(book);
+		allBooks.remove(book);
+	}
 	
 	
+	public void activate() {
+		active = true;	}
+	public void deactivate() {
+		active = false;	}
 	
 	
 	public String toString() {
-		String output = "";
+		String output = name + " has books ";
 		for(int i = 0; i < availBooks.size(); i++) {
-			output += availBooks + " ";
+			output += availBooks.get(i).getName() + " ";
 		}
+		output += " available and is ";
+		if(active)
+			output += "active";
+		else
+			output += "inactive";
 		return output;
 	}
 	
