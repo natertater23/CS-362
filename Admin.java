fb56c677e5cb418418101262ea4717ba81cbc041
jcastillo2583 <bluejccastle@gmail.com>
2019-05-05 20:20:09 -0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -2,43 +2,36 @@ import java.util.*;
 
 public class Admin {
 	private String name, email, password;
+	//undecided if id will be used
 	
-	public Student(String name, String email, String password) {
+	public Admin(String name, String email, String password) {
 		this.name = name;
 		this.email = email;
 		this.password = password;
 	}
+	
 
 	public String getName() {
-		return name;
-	}
-	
+		return name;	}
 	public String getEmail() {
-		return email;
-	}
-	
+		return email;	}
 	public String getPassword() {
-		return password;
-	}
+		return password;	}
 	
-	public void editPassword(String newPassword) {
-		email = newPassword;
-	}
 	
-	public void addCourse(Course course) {
-		//will need an array to add to
-	}
-	
-	public void removeCourse(Course course) {
-		course = null; //will need an array to remove from
-	}
-	
-	public void activateCourse() {
+	public void editProfile() {//Need to do
+		//change name
 		
+		//change email
+		
+		//change password
 	}
 	
-	public void deactivateCourse() {
-		
-	}
+	
+	public void activateCourse(Course course) {
+		course.activate();	}
+	public void deactivateCourse(Course course) {
+		course.deactivate();	}
+	
 
 }
