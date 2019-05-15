dfcd70a1c25634ea6f75d5afe32303a4a7a6ac00
jcastillo2583 <bluejccastle@gmail.com>
2019-05-14 23:30:58 -0700

    Update Student.java

--- Student.java
+++ Student.java
@@ -28,40 +28,16 @@ public class Student {
 		return id;	}
 	public String getPassword() {
 		return password;	}
-	public ArrayList getCourseArr(){
-		return courses;	}
-	public ArrayList getBookArr(){
-		return loanedBooks;	}
-	public int totalLoaned() {
-		return loanedBooks.size();	}
-	public int totalCourses() {
-		return courses.size();	}
-	
-	public void addCourse(Course course) {
-		courses.addCourse(course);	}
-	public void addBook(Book book) {
-		loanedBooks.addBook(book);	}
-	public void removeCourse(Course course) {
-		courses.removeCourse(course);	}
-	public void removeBook(Book book) {
-		loanedBooks.removeBook(book);	}
-
-	
-	public String displayLoanedBooks() {
-		return loanedBooks.displayBooks();	}
-	public String displayCourses() {
-		return courses.displayCourses();	}
 	
 		
 	public void loan(Book book) {//Need to do***
 		book.loan(this);
-		loanedBooks.add(book);
 	}
 	
 	
 	public String toString() {
 		if(!name.equals("Unavailable"))	//student exists
-			return (name + " " + id + password + "\n");
+			return (name + " " + id + " " + password + "\n");
 		return "N/A\n\n";
 	}
 
